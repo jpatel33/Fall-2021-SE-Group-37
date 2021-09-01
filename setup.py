594bbc06c07fd79f96a168cde7a4a2dc00a4af04
Akhil Gangarpu Sudhakar <akhil.clk93@gmail.com>
2021-09-01 18:58:29 -0400

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name: 'Fall-2021-SE-Group-37',
+    version: '0.1',
+    author: 'Fall-2021-SE-Group-37',
+    license: 'MIT',
+    packages: ['Fall-2021-SE-Group-37'],
+    classifiers: [
+        "License :: MIT License",
+        "Programming Language" :: "Python",
+        "Intended Audience" :: "Students",
+        "Topic" :: "Coursework"
+    ],
+    url:'https://github.com/jpatel33/Fall-2021-SE-Group-37'
+)
