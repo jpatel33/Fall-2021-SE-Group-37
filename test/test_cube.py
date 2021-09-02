010a282b5a25f2b15edeee3d511a898cddbb8f7f
Shubham Waghe <swaghe@ncsu.edu>
2021-09-01 21:27:56 -0400

    added more tc coverage

--- test/test_cube.py
+++ test/test_cube.py
@@ -1,7 +1,15 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from code import cubeme
+from code import cubeme, squareme, inc
 
 def testcube():
     assert cubeme(-2) == -8
+    assert cubeme(2) == 8
+
+def testsquare():
+    assert squareme(2) == 4
+    assert squareme(-2) == 4
+
+def test_inc():
+    assert inc(3) == 4
